404b9a1cb907164fa6bcb66141da2770d7ab0fe2
Genius <williamacarson1@outlook.com>
2020-05-18 16:30:13 -0400

    Initial commit
    Bazinga!

---
+++ Assets/Scenes/MyCustomBootStrap.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+class MyCustomBootStrap// : ICustomBootstrap
+{
+    public bool Initialize(string defaultWorldName)
+    {
+        Debug.Log("Executing bootstrap");
+        var world = new World("Custom world");
+        World.DefaultGameObjectInjectionWorld = world;
+        var systems = DefaultWorldInitialization.GetAllSystems(WorldSystemFilterFlags.Default);
+
+        var e = world.EntityManager.CreateEntity();
+
+        DefaultWorldInitialization.AddSystemsToRootLevelSystemGroups(world, systems);
+        ScriptBehaviourUpdateOrder.UpdatePlayerLoop(world);
+        return true;
+    }
+}
---
+++ Assets/Scenes/StructTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct StructTest : IFuck
+{
+    public int fuck;
+
+    public int DickButt()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public interface IFuck
+{
+    int DickButt();
+}
